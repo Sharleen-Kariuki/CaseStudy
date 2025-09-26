0002e3822a60912edfe59d7f9a0efff5492107a7
DANIEL LITUNYA <144170836+DANIELLITUNYA-CODER@users.noreply.github.com>
2025-09-26 13:04:05 +0300

    Update mpesa.js

--- routes/mpesa.js
+++ routes/mpesa.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const mpesaController = require("../controllers/mpesaController");
 
-// Initiate STK Push
+
 router.post("/stkpush", mpesaController.stkPush);
 
-// (Optional) Handle Quikk callback
 router.post("/callback", (req, res) => {
   // Save transaction result, update database, etc.
   console.log("Callback received:", req.body);
